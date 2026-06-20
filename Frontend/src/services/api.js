53be932cb097f8cdd746b05e0617bb08b7a6c050
piyushw09 <piyushw0622@gmail.com>
2026-06-20 12:01:05 +0530

    Frontedn updated

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -4,4 +4,28 @@ const API = axios.create({
     baseURL: "http://aws-3-tier-backend-new-env-env.eba-ijwip34e.ap-south-1.elasticbeanstalk.com/"
 });
 
+// Add JWT Token to Every Request
+API.interceptors.request.use(
+
+    (config) => {
+
+        const token = localStorage.getItem("token");
+
+        if (token) {
+
+            config.headers.Authorization = `Bearer ${token}`;
+
+        }
+
+        return config;
+    },
+
+    (error) => {
+
+        return Promise.reject(error);
+
+    }
+
+);
+
 export default API;
